0aa11a0f7081e4869bcfb171f7d2e2b723e24396
anand-yadav55 <anand55yadav@gmail.com>
2021-06-26 23:31:14 +0530

    0 suggestion null exception

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 export default function SearchBar(props) {
   const [input, setInput] = useState("");
   let [suggestionArray, set] = useState([]);
 
   function getSearchSuggestion() {
-    set(localStorage.getItem("imageFinderSearchHistoryData").split(","));
+    if (suggestionArray.length > 0) {
+      set(localStorage.getItem("imageFinderSearchHistoryData").split(","));
+    }
   }
 
   function AddSearchSuggestion(value) {
@@ -13,6 +15,7 @@ export default function SearchBar(props) {
     ifExist = ifExist ? ifExist.split(",") : [];
     ifExist.push(value);
     localStorage.setItem("imageFinderSearchHistoryData", ifExist);
+    set(ifExist);
     console.log(suggestionArray);
     getSearchSuggestion();
   }
@@ -20,16 +23,12 @@ export default function SearchBar(props) {
   function handleInputChange(e) {
     setInput(e.target.value.replaceAll(" ", "+"));
   }
-  useEffect(() => {
-    getSearchSuggestion();
-  }, []);
 
   const handleSearch = (e) => {
     e.preventDefault();
     AddSearchSuggestion(input);
     props.setPage((page) => 1);
     props.setSearchResult((prev) => []);
-    console.log(suggestionArray);
     let newURL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.REACT_APP_API}&tags=${input}&page=${props.page}&format=json&nojsoncallback=1`;
     props.setURL((prev) => {
       return newURL;
@@ -44,13 +43,13 @@ export default function SearchBar(props) {
           type="text"
           value={input}
           onChange={handleInputChange}
-          list="cars"
+          list="suggest"
         />
-        <datalist id="cars">
-          {console.log(suggestionArray.length)}
-          {suggestionArray.map((item) => {
-            return <option>{item}</option>;
-          })}
+        <datalist id="suggest">
+          {suggestionArray &&
+            suggestionArray.map((item, idx) => {
+              return <option key={idx}>{item}</option>;
+            })}
         </datalist>
         <button className="search_go">Go</button>
       </form>
